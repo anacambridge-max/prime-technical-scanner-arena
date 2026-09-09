7361bd57fa7394ad2db2312c0e3448424726393c
anacambridge-max <anacambridge@gmail.com>
2026-09-10 00:48:07 +0530

    Use multi-timeframe scanner dashboard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
-import Dashboard from "@/components/Dashboard";
+import MultiTimeframeDashboard from "@/components/MultiTimeframeDashboard";
 
 export const dynamic = "force-dynamic";
 
 export default function Page() {
-  return <Dashboard />;
+  return <MultiTimeframeDashboard />;
 }
